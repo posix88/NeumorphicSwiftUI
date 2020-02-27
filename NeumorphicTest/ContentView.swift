f689cd17ef0ca20d422c11bd6437cd3dfa1e8bcf
Antonino Francesco Musolino <antonino.musolino@kering.com>
2020-02-27 12:13:28 +0100

    Initial Commit

---
+++ NeumorphicTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeumorphicTest
+//
+//  Created by MUSOLINO Antonino on 27/02/2020.
+//  Copyright © 2020 Antonino Francesco Musolino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
